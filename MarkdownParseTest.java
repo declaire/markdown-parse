acd825dc8bd39bdd64d930e590c2777c2e2ae586
Claire <claireliu111@gmail.com>
2022-02-02 18:10:15 -0800

    updated JUnit tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,23 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(20, 1 + 1);
+        assertEquals(99999999, 1 + 1);
     }
+
+    /*
+    @Test
+    public void testFile1() throws IOException {
+        String contents= Files.readString(Path.of("C:/Users/clair/OneDrive/Documents/GitHub/markdown-parse/test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+        */
+    
 }
